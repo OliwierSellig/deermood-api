c7d3ca2d585dab8e89831feeb0f10b2ea2d21b23
OliwierSellig <oliwierandrzej.sellig@gmail.com>
2024-06-03 07:22:06 +0200

    feat: create simple routing && no route found error

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import Product from './models/productModel.js';
+import productRouter from './routes/productRoutes.js';
 
 export const app = express();
 
@@ -7,4 +8,15 @@ app.get('/api/home', async (req, res) => {
   const data = await Product.find();
 
   res.status(200).json({ status: 'succes', data });
+});
+
+app.use('/api/v1/products', productRouter);
+
+app.all('*', (req, res, next) => {
+  res
+    .status(404)
+    .json({
+      status: 'fail',
+      message: `Can't find ${req.originalUrl} on this server!`,
+    });
 });
