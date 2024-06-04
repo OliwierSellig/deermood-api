27accc392c2c92e5cb90eda1f813f3296c0fb96c
OliwierSellig <oliwierandrzej.sellig@gmail.com>
2024-06-04 07:06:27 +0200

    feat: add updateProduct & deleteProduct & getSingleProduct route functions

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -2,17 +2,15 @@ import Product from '../models/productModel.js';
 import AppError from '../utils/appError.js';
 import catchAsync from '../utils/catchAsync.js';
 
-export const getAllProducts = catchAsync(async (req, res) => {
+// ----------------------- Getting All Products ----------------------------
+
+export const getAllProducts = catchAsync(async (req, res, next) => {
   const products = await Product.find();
 
   res.status(200).json({ status: 'success', data: { products } });
 });
 
-export const createProduct = catchAsync(async (req, res) => {
-  const newProduct = await Product.create(req.body);
-
-  res.status(201).json({ status: 'succes', data: { newProduct } });
-});
+// ----------------------- Getting Single Product ----------------------------
 
 export const getSingleProduct = catchAsync(async (req, res, next) => {
   const id = req.params.id;
@@ -23,4 +21,43 @@ export const getSingleProduct = catchAsync(async (req, res, next) => {
   }
 
   res.status(200).json({ status: 'succes', data: { product } });
+});
+
+// ----------------------- Creating Product ----------------------------
+
+export const createProduct = catchAsync(async (req, res, next) => {
+  const newProduct = await Product.create(req.body);
+
+  res.status(201).json({ status: 'succes', data: { newProduct } });
+});
+
+// ----------------------- Updating Product ----------------------------
+
+export const updateProduct = catchAsync(async (req, res, next) => {
+  const id = req.params.id;
+  const product = await Product.findByIdAndUpdate(id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!product) {
+    return next(new AppError('No product found with that ID', 404));
+  }
+
+  res.status(200).json({ status: 'succes', data: { product } });
+});
+
+// ----------------------- Deleting Product ----------------------------
+
+export const deleteProduct = catchAsync(async (req, res, next) => {
+  const id = req.params.id;
+  const product = await Product.findByIdAndDelete(id);
+  if (!product) {
+    return next(new AppError('No product found with that ID', 404));
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
 });
--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,14 +1,20 @@
 import express from 'express';
 import {
   createProduct,
+  deleteProduct,
   getAllProducts,
   getSingleProduct,
+  updateProduct,
 } from '../controllers/productController.js';
 
 const router = express.Router();
 
 router.route('/').get(getAllProducts).post(createProduct);
 
-router.route('/:id').get(getSingleProduct);
+router
+  .route('/:id')
+  .get(getSingleProduct)
+  .patch(updateProduct)
+  .delete(deleteProduct);
 
 export default router;
