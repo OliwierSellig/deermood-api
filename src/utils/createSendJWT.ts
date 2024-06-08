c31764e9ced539acbad2b10c1d734978dca8c108
OliwierSellig <oliwierandrzej.sellig@gmail.com>
2024-06-08 10:30:11 +0200

    feat: implement cookie creation with JWT

--- src/utils/createSendJWT.ts
+++ src/utils/createSendJWT.ts
@@ -1,5 +1,11 @@
 import { Response } from 'express';
-import { signJWT } from './signJWT.js';
+import jwt, { Secret } from 'jsonwebtoken';
+
+const signJWT = (id: string) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET as Secret, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
 
 export const createSendJWT = (
   data: object | null,
@@ -8,6 +14,17 @@ export const createSendJWT = (
   res: Response,
 ) => {
   const token = signJWT(userId);
+
+  const cookieOptions: { expires: Date; secure?: boolean; httpOnly: boolean } =
+    {
+      expires: new Date(
+        Date.now() +
+          Number(process.env.JWT_COOKIE_EXPIRES_IN) * 24 * 60 * 60 * 1000,
+      ),
+      httpOnly: true,
+    };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  res.cookie('jwt', token, cookieOptions);
 
   res
     .status(statusCode)
--- src/utils/signJWT.ts
+++
@@ -1,7 +0,0 @@
-import jwt, { Secret } from 'jsonwebtoken';
-
-export const signJWT = (id: string) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET as Secret, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
-};
